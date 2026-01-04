95eaa2ff2537a9056b008d1a59b66e915a79f213
Rohan Shaw <deathstar@Mac.lan>
2026-01-05 04:05:14 +0530

    add reminder logic and ui with improvements in css and js files

--- todo.js
+++ todo.js
@@ -1,58 +1,42 @@
-const text = document.querySelector("#input-box");
-const btn = document.querySelector("#btn");
-const list = document.querySelector("#list-container");
+const text = document.querySelector(".input-box input");
+const addBtn = document.querySelector("#add");
+const todoList = document.querySelector(".list");
+let total = document.querySelector("#total");
+const EmptyImg = document.querySelector(".list img");
+const EmptyMsg = document.querySelector(".list p");
+let idxtotal =0;
 
 
-btn.addEventListener("click",()=>{
-    const task = text.value;
-    if(task===""){
-        alert("Please Enter a Task");
-        return;
-    }
-    const listitem = document.createElement("li");
-    listitem.innerText = task;
-   
-
-    listitem.addEventListener("click",()=>{
-        listitem.classList.toggle("styles");
-        saveData();
-    })
+addBtn.addEventListener("click",()=>{
+    AddTasks();
     
+})
 
-
-    listitem.addEventListener("dblclick",()=>{
-        listitem.remove();
-        saveData();
-    });
-    list.appendChild(listitem);
-    saveData();
-    text.value="";
-
-});
+const AddTasks = ()=>{
+    let userInput = text.value.trim();
+    if(userInput.length===0){
+        alert("Please enter a reminder!");
+        return;
+    }else{
+        let li =document.createElement("li");
+        idxtotal++;
+        EmptyImg.classList.add("hidden");
+        EmptyMsg.classList.add("hidden");
+        li.classList.add("task-item");
+        li.innerHTML=
+        `<div class="task-content">
+            <input type="checkbox" class="task-checkbox">
+            <span>${userInput}</span>
+        </div>
+            `;
+        total.innerText=idxtotal
+        todoList.appendChild(li);
+        text.value ="";
+    }
+}
 
 text.addEventListener("keydown",(evt)=>{
     if(evt.key==="Enter"){
-        btn.click();
+        addBtn.click();
     }
 })
-
-function saveData(){
-    localStorage.setItem('data',list.innerHTML);
-}
-
-function showData(){
-    list.innerHTML = localStorage.getItem("data");
-    const allItems = list.querySelectorAll("li");
-    allItems.forEach((item)=>{
-        item.addEventListener("click",()=>{
-            item.classList.toggle("styles");
-            saveData();
-        })
-
-        item.addEventListener("dblclick",()=>{
-            item.remove();
-            saveData();
-        })
-    })
-}
-showData();
