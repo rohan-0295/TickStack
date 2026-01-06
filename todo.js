f1f693fc21ab8d92ecb5510bb8a56dbb32c0bac8
Rohan Shaw <deathstar@Mac.lan>
2026-01-06 05:54:20 +0530

    implement task filtering, dynamic stat updates, and UI styling

--- todo.js
+++ todo.js
@@ -4,11 +4,20 @@ const todoList = document.querySelector(".list");
 let total = document.querySelector("#total");
 const EmptyImg = document.querySelector(".list img");
 const EmptyMsg = document.querySelector(".list p");
+const all = document.querySelector("#all");
+const active = document.querySelector("#active");
+const completed = document.querySelector("#complete");
+all.classList.add("select");
+
+
+
+console.log(completed); 
 let idxtotal =0;
 
 
 addBtn.addEventListener("click",()=>{
     AddTasks();
+
     
 })
 
@@ -27,16 +36,73 @@ const AddTasks = ()=>{
         `<div class="task-content">
             <input type="checkbox" class="task-checkbox">
             <span>${userInput}</span>
-        </div>
-            `;
-        total.innerText=idxtotal
+        </div>`;
+        const checkbox = li.querySelector(".task-checkbox");
+        checkbox.addEventListener("change",()=>{
+            li.classList.toggle("completed");
+            updateStats();
+        });
         todoList.appendChild(li);
+        updateStats();
         text.value ="";
     }
+}
+
+
+const updateStats = ()=>{
+    const allTasks = document.querySelectorAll(".task-item");
+    const completedTasks = document.querySelectorAll(".task-item.completed");
+
+    const totalCount = allTasks.length;
+    const CompletedCount = completedTasks.length;
+    const pendingCount = totalCount- CompletedCount
+
+    total.innerText= totalCount;
+    document.querySelector("#completed").innerText = CompletedCount;
+    document.querySelector("#pending").innerText = pendingCount;
 }
 
 text.addEventListener("keydown",(evt)=>{
     if(evt.key==="Enter"){
         addBtn.click();
     }
+})
+
+active.addEventListener("click",()=>{
+    active.classList.add("select");
+    all.classList.remove("select");
+    completed.classList.remove("select");
+    const allTasks = document.querySelectorAll(".task-item");
+    allTasks.forEach((task)=>{
+        if(!task.classList.contains("completed")){
+            task.style.display="flex";
+        }else{
+            task.style.display="none";
+        }
+    })
+
+})
+
+all.addEventListener("click",()=>{
+    all.classList.add("select");
+    active.classList.remove("select");
+    completed.classList.remove("select");
+    const allTasks = document.querySelectorAll(".task-item");
+    allTasks.forEach((task)=>{
+        task.style.display="flex";
+    })
+})
+
+completed.addEventListener("click",()=>{
+    completed.classList.add("select");
+    all.classList.remove("select");
+    active.classList.remove("select");
+    const allTasks = document.querySelectorAll(".task-item");
+    allTasks.forEach((task)=>{
+        if(task.classList.contains("completed")){
+            task.style.display="flex";
+        }else{
+            task.style.display="none";
+        }
+    })
 })
